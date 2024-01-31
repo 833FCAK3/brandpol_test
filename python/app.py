1399ba0f5742f4688d94c97d36fc207a1bf4119f
Artyom Kobzar <a.kobzar1995@gmail.com>
2024-01-31 13:34:08 +0500

    Add write and read from db

--- python/app.py
+++ python/app.py
@@ -1,11 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from database import get_postgres_db
+from models import Greeting
 
 app = FastAPI()
 
+
 @app.get("/greet")
-def greet(name: str = "пользователь"):
-    return f"Привет, {name.capitalize()}, от Python!"
+def greet(name: str = "пользователь", db: Session = Depends(get_postgres_db)):
+    name = name.capitalize()
+    db_greeting = Greeting(name=name)
+    db.add(db_greeting)
+    db.commit()
+    return f"Привет, {name}, от Python!"
+
 
 @app.get("/greet/history")
-def get_history():
-    pass
+def get_history(db: Session = Depends(get_postgres_db)):
+    return db.query(Greeting).all()
--- python/models.py
+++ python/models.py
@@ -2,7 +2,7 @@
 from sqlalchemy import Column, Integer, String, DateTime, func
 from database import Base
 
-class greetings(Base):
+class Greeting(Base):
     __tablename__ = "greetings"
 
     id = Column(Integer, primary_key=True, index=True)
