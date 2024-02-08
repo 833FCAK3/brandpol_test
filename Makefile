0f94d28f53ef7d639b8adf1636bcab4f7984b16a
Artyom Kobzar <a.kobzar1995@gmail.com>
2024-02-08 21:49:49 +0500

    Drop go test db after tests, add devtestgo and devtestpy make commands

--- Makefile
+++ Makefile
@@ -23,10 +23,16 @@ testgo:
 	docker-compose run go_app go test -v
 
 testpy:
-	docker-compose run py_app pytest
+	docker-compose run py_app pytest -v
 
 devgo:
-	docker-compose run --rm --volume=${CURDIR}/src/go_app:/src/go_app go_app sh -c "go run app.go config.go"
+	docker-compose run --rm --volume=${CURDIR}/src/go_app:/src/go_app go_app sh -c "/wait && go run app.go config.go"
+
+devtestgo:
+	docker-compose run --rm --volume=${CURDIR}/src/go_app:/src/go_app go_app sh -c "/wait && go test -v"
 
 devpy:
 	docker-compose run --rm --volume=${CURDIR}/src/py_app:/src/py_app py_app bash -c "uvicorn app:app --host 0.0.0.0 --port ${PY_PORT}"
+
+devtestpy:
+	docker-compose run --rm --volume=${CURDIR}/src/py_app:/src/py_app py_app pytest -v
--- src/go_app/app_test.go
+++ src/go_app/app_test.go
@@ -13,9 +13,8 @@ var testDB *gorm.DB
 
 func TestMain(m *testing.M) {
 	// Set up test database
-	testDbName := "postgres_test"
 	var err error
-	testDB, err = createTestDatabase(testDbName)
+	testDB, err = createTestDatabase(Test_db)
 	if err != nil {
 		fmt.Printf("Error creating test database: %v\n", err)
 		os.Exit(1)
@@ -24,16 +23,16 @@ func TestMain(m *testing.M) {
 	// Run tests
 	exitCode := m.Run()
 
-	// // Clean up test database
-	// if err := dropTestDatabase(testDB, testDbName); err != nil {
-	// 	fmt.Printf("Error dropping test database %v: %v\n", testDbName, err)
-	// 	os.Exit(1)
-	// }
+	// Clean up test database
+	if err := dropTestDatabase(Test_db); err != nil {
+		fmt.Printf("Error dropping test database %v: %v\n", Test_db, err)
+		os.Exit(1)
+	}
 
 	os.Exit(exitCode)
 }
 
-func createTestDatabase(DbName string) (*gorm.DB, error) {
+func createTestDatabase(dbname string) (*gorm.DB, error) {
 	// Connect to the PostgreSQL server
 	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", DbHost, DbPort, DbUser, DbPassword)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
@@ -42,14 +41,14 @@ func createTestDatabase(DbName string) (*gorm.DB, error) {
 	}
 
 	var count int64
-	db.Raw("SELECT COUNT(*) FROM pg_database WHERE datname = ?", DbName).Scan(&count)
+	db.Raw("SELECT COUNT(*) FROM pg_database WHERE datname = ?", dbname).Scan(&count)
 	if count > 0 {
 		// Database already exists, return without creating it
 		return nil, nil
 	}
 
 	// Create the test database
-	rawSQL := fmt.Sprintf("CREATE DATABASE %s", DbName)
+	rawSQL := fmt.Sprintf("CREATE DATABASE %s", dbname)
 	if err := db.Exec(rawSQL).Error; err != nil {
 		return nil, err
 	}
@@ -62,4 +61,21 @@ func createTestDatabase(DbName string) (*gorm.DB, error) {
 	}
 
 	return db, nil
+}
+
+func dropTestDatabase(dbName string) error {
+	// Connect to the PostgreSQL server
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", DbHost, DbPort, DbUser, DbPassword)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+
+	// Drop the test database
+	result := db.Exec(fmt.Sprintf("DROP DATABASE IF EXISTS %s WITH (FORCE)", dbName))
+	if result.Error != nil {
+		fmt.Printf("Error dropping test database %v: \n", dbName)
+		return result.Error
+	}
+	return nil
 }
